085e1ec84c5978cb1efc14f4d263e10e46041601
EBign <92940020+EBign@users.noreply.github.com>
2021-10-29 01:02:31 +0200

    Add files via upload

--- var.js
+++ var.js
@@ -1,6 +1,13 @@
-(function($)){
-    $('#bouttonnav').click(function(e)){
-        e.preventdefault();
-        $('body').toggleClass('with--sidebar');
-    }
-})(jQuery)
+function togglemenu() {
+    const header = document.querySelector ('header');
+    const navbar = document.querySelector ('nav');
+    const ul = document.querySelector ('ul');
+    const burger = document.querySelector ('.bouttonnav');
+    burger.addEventListener('click', (e) => {
+        header.classList.toggle('montreheader');
+        ul.classList.toggle('montreul');
+        navbar.classList.toggle('montrenav');
+        burger.classList.toggle('croix');
+    });
+}
+togglemenu();
